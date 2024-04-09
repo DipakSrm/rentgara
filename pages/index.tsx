82629e136dea5a40e4da17770ee26045ca6c1bdd
Dipak99990 <dipaksrm98@gmail.com>
2024-04-09 16:28:55 +0545

    recent

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,6 @@ import { Inter } from "next/font/google";
 
 export default function Home() {
   return (
-    <p>THis is dipak branch</p>
+    <h1>Hellpo everyone this is jus an exmaplke of dipak</h1>
   );
 }
